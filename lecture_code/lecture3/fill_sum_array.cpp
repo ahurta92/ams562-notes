17f2a1b59152453f0419d7aa33227fdce8ad2d55
Adrian Hurtado <ahurta92@gmail.com>
2021-09-19 12:54:06 -0400

    some lecture changes

--- lecture_code/lecture3/fill_sum_array.cpp
+++ lecture_code/lecture3/fill_sum_array.cpp
@@ -6,17 +6,21 @@ int main(int argc, char **argv) {
 
   double *data = new double[argc - 1]; // pointer to double array
                                        // heap allocation
+                                       //
 
   // fill with  with argv
   for (int i = 0; i < argc; ++i) {
-    data[i] = atof(argv[i]);
+    data[i] = atof(argv[i + 1]);
   }
-
-  for (int i = 1; i < argc; ++i) {
+  int i = 1;
+  for (; i < argc;) {
     std::cout << data[i] << std::endl;
+    ++i
   }
+  std::cout << i << std::endl;
+
   double sum = 0;
-  for (int i = 1; i < argc; ++i) {
+  for (int i = 0; i < argc - 1; ++i) {
     sum += data[i];
   }
   std::cout << "sum of data   " << sum << std::endl;
--- lecture_code/lecture3/struct.cpp
+++ lecture_code/lecture3/struct.cpp
@@ -8,7 +8,6 @@ struct StructName {
 }; //<-- don't forget teh semicolon
 
 struct ComplexNumber {
-
   double real;
   double imag;
 };
@@ -17,25 +16,39 @@ int main() {
 
   ComplexNumber a, b;
 
+  // ComplexNumber c{1, 2};
+
   // a.real using accessing operator
   a.real = 1.0;
   a.imag = -1.0;
+
   b.real = 2.0;
   b.imag = 1.0;
 
   std::cout << sizeof(double) << std::endl;
   std::cout << sizeof(ComplexNumber) << std::endl;
-
-  ComplexNumber *ptr_a = &a;
+  //
+  ComplexNumber *ptr_a = &a; // ptr_a points to address of a
   ComplexNumber *ptr_b = &b;
+
+  int z = 20;
+  int *ptr_z = &z;
+  *ptr_z;
+
+  ptr_a[0].real = 20; //
+  ptr_a->imag;        ///
+  ptr_a->real;
 
   std::cout << "complex a=(" << ptr_a->real << ',' << ptr_a->imag << ")\n";
   std::cout << "complex b=(" << ptr_b->real << ',' << ptr_b->imag << ")\n";
+  // heap allocation
+  //
 
   ComplexNumber *ptr_array = new ComplexNumber[10];
 
+  // fill
   for (int i = 0; i != 10; ++i) {
-    ptr_array[i].real = 1.0;
+    ptr_array[i].real = 1.0; //
     ptr_array[i].imag = 1.0;
   }
 
@@ -45,7 +58,7 @@ int main() {
   }
 
   for (int i = 0; i != 10; ++i) {
-    (i + ptr_array)->real = 2.0;
+    (i + ptr_array)->real = 2.0; // ptr_array[i].real
     (i + ptr_array)->imag = 2.0;
   }
 
