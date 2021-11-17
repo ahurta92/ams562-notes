af3d2e4a528148b0dc7b4a89bcd4c0587f384766
Adrian Hurtado <ahurta92@gmail.com>
2021-11-17 14:27:46 -0500

    move constructor

--- hw/polynomial/polynomial.h
+++ hw/polynomial/polynomial.h
@@ -4,46 +4,68 @@
 
 #ifndef LECTURE_POLYNOMIAL_H
 #define LECTURE_POLYNOMIAL_H
-#include <vector>
 #include <iostream>
+#include <vector>
 
-class Polynomial{
+class Polynomial
+{
 private:
 	int order;
-	 std::vector<double> coeffs;
- public:
+	std::vector<double> coeffs;
 
-	 explicit Polynomial(int o);
-	 ~Polynomial() = default;
-	 double operator[](int o) const{
-		 //return coeffs[o];//.at(o);// no bounds checking
-		 return coeffs.at(o);// with bound checking
-	 }
-	double& operator[](int o) {
-		std::cout<<"this one";
-		return coeffs.at(o);
-
+public:
+	explicit Polynomial(int o);
+	double operator[](int o) const
+	{
+		//return coeffs[o];//.at(o);// no bounds checking
+		return coeffs.at(o); // with bound checking
 	}
-	Polynomial(std::initializer_list<double> l );
+	double& operator[](int o)
+	{
+		std::cout << "this one";
+		return coeffs.at(o);
+	}
+	Polynomial(const Polynomial& p1):order{p1.order}, coeffs{p1.coeffs }{
+		std::cout<<"copy constructor"<<std::endl;
+	} ;
+	Polynomial& operator=(const Polynomial& p1){
 
-	int size() const {
+		order=p1.order;
+		coeffs = p1.coeffs;
+		std::cout<<"copy assignment"<<std::endl;
+	}
+
+
+	Polynomial& operator=(Polynomial&& p1)
+	{
+		order = p1.size();
+		coeffs = std::move(p1.coeffs);
+		std::cout << "operator=(&&p)";
+		return *this;
+	}
+	Polynomial(std::initializer_list<double> l);
+
+	int size() const
+	{
 		return order;
 	}
-	auto begin() const{
+	auto begin() const
+	{
 		return coeffs.begin();
 	}
-	auto end() const{
+	auto end() const
+	{
 		return coeffs.end();
 	}
-	auto begin() {
+	auto begin()
+	{
 		return coeffs.begin();
 	}
-	auto end() {
+	auto end()
+	{
 		return coeffs.end();
 	}
-
-
 };
 
-std::ostream& operator<<(std::ostream& os,const Polynomial & p1);
+std::ostream& operator<<(std::ostream& os, const Polynomial& p1);
 #endif //LECTURE_POLYNOMIAL_H
