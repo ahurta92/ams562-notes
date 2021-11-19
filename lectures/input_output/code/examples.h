f14bf66d7965042ff934c2d7668c28a06129bb8c
Adrian Hurtado <ahurta92@gmail.com>
2021-11-19 12:47:41 -0500

    Create examples.h
    input output

---
+++ lectures/input_output/code/examples.h
@@ -0,0 +1,48 @@
+//
+// Created by adrian on 11/4/21.
+//
+
+#ifndef LECTURE_EXAMPLES_H
+#define LECTURE_EXAMPLES_H
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <algorithm>
+
+
+using namespace std;
+
+struct Entry{
+	string name;
+	int number;
+};
+	//  the one we use for the initialization in code
+ostream &operator<<(ostream &os, const Entry &e);
+
+ostream &operator<<(ostream &os, const vector<Entry> &v);
+istream &operator>>(istream &is, Entry &e);
+ostream &operator<<(ostream &os, const list<Entry> &lst);
+
+template <typename T>
+ostream &operator<<(ostream &os,const vector<T> &v){
+	for (auto&e:v){
+		os<<e<<" ";
+	}
+	os<<"\n";
+	return os;
+}
+
+vector<Entry> readEntries(istream &is);
+vector<Entry> readFileEntries(const string& f_name);
+map<string,int> readTextMap(const string & f_name);
+map<string,int> readNameAgeData(const string & f_name);
+
+vector<int> read_ints(istream &is);
+
+vector <int> read_ints(istream &is,const string&terminator);
+
+
+
+#endif //LECTURE_EXAMPLES_H
